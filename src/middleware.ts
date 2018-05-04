c0b4dcb913bf6764908aaa907793d8ca383e2235
nnecec <nnecec@outlook.com>
2018-05-04 20:42:12 +0800

    remove koa-convert

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 import * as compose from 'koa-compose'
-import * as convert from 'koa-convert'
+// import * as convert from 'koa-convert'
 import * as cors from '@koa/cors'
 import * as bodyParser from 'koa-bodyparser'
 import * as logger from 'koa-logger'
